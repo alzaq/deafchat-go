fe793509129a57a7d1aac47a9039dd5aae1daa6c
alzaq <ales@tapmates.com>
2018-04-10 16:43:06 +0200

    vision new url

--- vision/vision.go
+++ vision/vision.go
@@ -2,7 +2,9 @@ package vision
 
 import (
 	"context"
-	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 
 	vision "cloud.google.com/go/vision/apiv1"
@@ -15,30 +17,53 @@ func VisionInit() {
 }
 
 func DetectURL(url string) string {
-	ctx := context.Background()
 
-	fmt.Println(url)
+	// don't worry about errors
+	response, e := http.Get(url)
+	if e != nil {
+		log.Fatal(e)
+	}
+
+	defer response.Body.Close()
+
+	//open a file for writing
+	f, err := os.Create("/photos/random")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Use io.Copy to just dump the response body to the file. This supports huge files
+	_, err = io.Copy(f, response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	ctx := context.Background()
 
 	client, err := vision.NewImageAnnotatorClient(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	image := vision.NewImageFromURI(url)
+	image, err := vision.NewImageFromReader(f)
+	if err != nil {
+		panic(err)
+	}
 	annotations, err := client.DetectLabels(ctx, image, nil, 10)
 	if err != nil {
 		panic(err)
 	}
 
-	response := ""
+	text := ""
 
 	if len(annotations) == 0 {
-		response = "No labels found."
+		text = "No labels found."
 	} else {
-		response = "Labels:"
+		text = "Labels:"
 		for _, annotation := range annotations {
-			response += annotation.Description
+			text += annotation.Description
 		}
 	}
-	return response
+
+	return text
 }
